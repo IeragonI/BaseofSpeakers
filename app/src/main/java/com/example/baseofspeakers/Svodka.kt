86bed5fd3a5218bc4a33af932a10bc012766d383
IeragonI <eragon2007d@gmail.com>
2023-11-11 23:06:57 +0600

    New activity

---
+++ app/src/main/java/com/example/baseofspeakers/Svodka.kt
@@ -0,0 +1,11 @@
+package com.example.baseofspeakers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Svodka : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_svodka)
+    }
+}
